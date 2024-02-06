a395e7ba6330f522806390b0ecb92ed287a8dc3d
ianburrows04 <ianburrows777@gmail.com>
2024-02-05 16:49:29 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,33 @@
+num1 = int(input("Enter you first number: "))
+op = input("Enter your operator: ")
+num2 = int(input("Enter your second number: "))
 
+if op == "+" or op == "-" or op == "/" or op == "*" or op == "**" or op == "//" or op == "%":
+	if op == "+":
+		print(f"{num1} + {num2} = {num1 + num2}")
+
+	elif op == "-":
+		print(f"{num1} - {num2} = {num1 - num2}")
+
+	elif op == "*":
+		print(f"{num1} * {num2} = {num1 * num2}")
+
+	elif op == "/":
+		if num2 == 0:
+			print("Cannot divide by zero, the answer is undefined.")
+		else:
+			print(f"{num1} / {num2} = {num1 / num2}")
+	
+	elif op == "**":
+		print(f"{num1} ** {num2} = {num1 ** num2}")
+
+	elif op == "//":
+		if num2 == 0:
+			print("Cannot divide by zero, the answer is undefined.")
+		else:
+			print(f"{num1} // {num2} = {num1 // num2}")
+	elif op == "%":
+		print(f"{num1} % {num2} = {num1 % num2}")
+
+else:
+	print("Invalid operator.")
